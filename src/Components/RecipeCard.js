5b4453795e7045ea9990a5da980af26e7c8f78aa
Steph Luo <steph.luokl@gmail.com>
2022-10-04 00:32:15 +0800

    Update RecipeCard.js

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Ingredients from "./Ingredients";
 import Instructions from "./Instructions";
+import { Link } from "react-router-dom";
 
 // component that renders the recipe basic information after search
 
@@ -8,21 +9,29 @@ const RecipeCard = (props) => {
   const ingredientsList = props.ingredients;
   const instructionsList = props.instructions;
 
-  console.table(instructionsList);
+  // console.table(instructionsList);
 
   // return <></>;
 
   return (
     <div className="recipeContainer" id={props.id}>
       <img src={props.src} alt={props.alt} />
-      <h3>{props.alt}</h3>
+      <h3>
+        {props.recipeName.lenght < 20
+          ? `${props.recipeName}`
+          : `${props.recipeName.substring(0, 25)}...`}
+      </h3>
       <p>description: {props.description}</p>
       <p>yield: {props.servingSize} servings</p>
       <p>prep time: {props.prepTime}</p>
       <p>cook time: {props.cookTime} mins</p>
       <Ingredients ingredientsList={ingredientsList} />
       <Instructions instructionsList={instructionsList} />
-      <button>View More</button>
+      <button>
+        <Link to={`/recipeDetails/${props.id}/${props.recipeName}`}>
+          View More
+        </Link>
+      </button>
     </div>
   );
 };
