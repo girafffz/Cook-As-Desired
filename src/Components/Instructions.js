851fadae15eccc5794551b6315f004777c92df67
Steph Luo <steph.luokl@gmail.com>
2022-10-04 02:55:38 +0800

    instructions as ordered list completed

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -1,11 +1,27 @@
 import React from "react";
 
+// returns ordered list of instructions to be shown in recipe details
+
 const Instructions = (props) => {
-  const instructionsList = props.instructionsList;
+  const instructionsArr = props.instructions;
 
-  console.log(instructionsList);
+  const stepsArr = [];
 
-  return <>instructions: {JSON.stringify(instructionsList)}</>;
+  const getEachSteps = () => {
+    for (const step in instructionsArr) {
+      stepsArr.push(<li>{instructionsArr[step].display_text}</li>);
+    }
+    return stepsArr;
+  };
+
+  getEachSteps();
+
+  return (
+    <div>
+      <h3>Instructions</h3>
+      <ol>{stepsArr}</ol>
+    </div>
+  );
 };
 
 export default Instructions;
