a0206eea1c03f95870af2878468fc7501cb559a3
Steph Luo <steph.luokl@gmail.com>
2022-10-05 02:17:33 +0800

    Update Ingredients.js

--- src/Components/Ingredients.js
+++ src/Components/Ingredients.js
@@ -1,11 +1,30 @@
 import React from "react";
 
+// Ingredients shows individual ingredient of the selected recipe
+
 const Ingredients = (props) => {
-  const ingredientsList = props.ingredientsList;
+  // getting the data containing ingredients of the selected recipe
+  const componentsArr = props.ingredientsList;
 
-  console.log(ingredientsList);
+  const ingredientArr = [];
+  // looping and pushing each ingredient into an array
+  const getEachIngredient = () => {
+    if (Array.isArray(componentsArr)) {
+      for (const item of componentsArr[0].components) {
+        ingredientArr.push(<li key={item.id}>{item.raw_text}</li>);
+      }
+    }
+    return ingredientArr;
+  };
 
-  return <div>ingredients: {JSON.stringify(ingredientsList)}</div>;
+  getEachIngredient();
+
+  return (
+    <div>
+      <h3>Ingredients</h3>
+      <ol>{ingredientArr}</ol>
+    </div>
+  );
 };
 
 export default Ingredients;
