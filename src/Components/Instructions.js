ddd69bb1092986d9054a4504f9c30e484f3008a1
Steph Luo <steph.luokl@gmail.com>
2022-10-05 02:18:01 +0800

    Update Instructions.js

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -3,13 +3,18 @@ import React from "react";
 // returns ordered list of instructions to be shown in recipe details
 
 const Instructions = (props) => {
+  // getting data containing instructions of the selected recipe
   const instructionsArr = props.instructionsList;
 
   const stepsArr = [];
-
+  // mapping each steps of the cooking instruction into an array
   const getEachSteps = () => {
     for (const step in instructionsArr) {
-      stepsArr.push(<li>{instructionsArr[step].display_text}</li>);
+      stepsArr.push(
+        <li key={instructionsArr[step].id}>
+          {instructionsArr[step].display_text}
+        </li>
+      );
     }
     return stepsArr;
   };
