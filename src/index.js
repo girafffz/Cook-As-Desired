d52081f681a3d88d24dc09b60bde1ac7f7a840b8
Steph Luo <steph.luokl@gmail.com>
2022-10-07 05:46:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import Router from "./Components/Router";
+import App from "./App";
 
-ReactDOM.render(<Router />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
