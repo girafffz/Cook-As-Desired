d2e60e746646c8def7694d66932fb5cd80cc4dfb
Steph Luo <steph.luokl@gmail.com>
2022-10-07 05:45:51 +0800

    restructure info layout

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -3,23 +3,36 @@ import React from "react";
 // RecipeCard renders basic information of each recipe relevant to search
 
 const RecipeCard = (props) => {
+  // console.log(props);
+
   return (
-    <div className="recipeContainer" id={props.id}>
-      <img src={props.src} alt={props.alt} />
-      <h3>
-        {props.recipeName.length < 20
-          ? `${props.recipeName}`
-          : `${props.recipeName.substring(0, 25)}...`}
-      </h3>
-      <p>{`Servings: ${props.servingSize}`}</p>
-      <p>{`Prep: ${props.prepTime} mins`}</p>
-      <p>{`Cook: ${props.cookTime} mins`}</p>
-      <div>
-        <button type="button" onClick={props.onClick}>
-          View More
-        </button>
+    // <div className="row">
+    <div className="recipe-container">
+      <div className="card" id={props.id}>
+        <img
+          className="card-img-top card-img"
+          src={props.src}
+          alt={props.alt}
+        />
+        <div className="card-body">
+          {/* Card Content */}
+          <h5 className="card-title mb-3">
+            {props.recipeName.length < 20
+              ? `${props.recipeName}`
+              : `${props.recipeName.substring(0, 21)}...`}
+          </h5>
+          <div className="card-text mb-1">{`Servings: ${props.servingSize}`}</div>
+          <div className="card-text mb-1">{`Prep: ${props.prepTime} mins`}</div>
+          <div className="card-text mb-1">{`Cook: ${props.cookTime} mins`}</div>
+          <div>
+            <button type="button" className="btn mt-3" onClick={props.onClick}>
+              View More
+            </button>
+          </div>
+        </div>
       </div>
     </div>
+    // </div>
   );
 };
 
