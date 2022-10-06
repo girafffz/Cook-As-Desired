b3c24c5314f3a7052718bfd3cf3c5e9b2b42d178
Steph Luo <steph.luokl@gmail.com>
2022-10-07 05:44:12 +0800

    Update RecipeDetails.js

--- src/Components/RecipeDetails.js
+++ src/Components/RecipeDetails.js
@@ -10,18 +10,25 @@ const RecipeDetails = (props) => {
   const recipeSelected = props.recipe;
 
   return (
-    <div id={recipeSelected.id}>
-      <img src={recipeSelected.thumbnail_url} alt={recipeSelected.name}></img>
-      <h2>{recipeSelected.name}</h2>
-      <p>{`Servings: ${recipeSelected.num_servings}`}</p>
-      <p>{`Prep: ${recipeSelected.prep_time_minutes} mins`}</p>
-      <p>{`Cook: ${recipeSelected.cook_time_minutes} mins`}</p>
-      <p>{recipeSelected.description}</p>
-      <Ingredients ingredientsList={recipeSelected.sections} />
-      <Instructions instructionsList={recipeSelected.instructions} />
-      <button type="button" onClick={props.onClick}>
-        Back
-      </button>
+    <div className="container">
+      <div id={recipeSelected.id}>
+        <img
+          className="detail-img"
+          src={recipeSelected.thumbnail_url}
+          alt={recipeSelected.name}
+        ></img>
+        <h2 className="mb-4">{recipeSelected.name}</h2>
+        <p>{`Servings: ${recipeSelected.num_servings}`}</p>
+        <p>{`Prep: ${recipeSelected.prep_time_minutes} mins`}</p>
+        <p>{`Cook: ${recipeSelected.cook_time_minutes} mins`}</p>
+        <h3 className="mb-4 mt-4">Description</h3>
+        <p>{recipeSelected.description}</p>
+        <Ingredients ingredientsList={recipeSelected.sections} />
+        <Instructions instructionsList={recipeSelected.instructions} />
+        <button className="btn mt-5" type="button" onClick={props.onClick}>
+          Back
+        </button>
+      </div>
     </div>
   );
 };
